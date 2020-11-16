11f95e896505f53d73b2b0bf4fcf366e686b0438
lusteiger <leonardoxy7@gmail.com>
2020-11-16 20:17:51 +0800

    finish the practice1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import { REGION, ADDRESS, PORT, PATH } from "./constant";
+import getURL from "./utils";
 
+const url = getURL(ADDRESS, PORT, PATH);
+const region = REGION;
+
+export { url, region };
