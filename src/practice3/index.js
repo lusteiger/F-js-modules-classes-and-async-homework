4b7e51484fcb4853029e37511edb1fd2c258f075
lusteiger <leonardoxy7@gmail.com>
2020-11-16 21:15:02 +0800

    finish the practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,11 @@
-export const getPoetry
+import { Person } from "./Person";
+export class Student extends Person {
+  constructor(name, className) {
+    super(name);
+    this.className = className;
+  }
+  study = () => {
+    this.move();
+    console.log(`${this.name} is studying in ${this.className}`);
+  };
+}
